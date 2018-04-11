93f8545d5e59947fbed4d065bf523f26a3c27671
AsyncVoid <cookieassassin32@gmail.com>
2018-04-11 23:28:23 +0100

    Initial Setup

---
+++ src/main/java/me/Async/h2m/Hard2Mine.java
@@ -0,0 +1,33 @@
+package me.Async.h2m;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Hard2Mine.MODID, version = Hard2Mine.VERSION)
+public class Hard2Mine
+{
+    public static final String MODID = "hard2mine";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+    	
+	}
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
+    
+    @EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+    	
+	}
+}
